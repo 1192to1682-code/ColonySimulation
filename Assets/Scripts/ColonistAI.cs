e518e951a4a08532c80f45ab66e106b92e46cbea
1192to1682-code <1192to1682@gmail.com>
2025-11-30 12:50:19 +0900

    7回目：ハンガーバーとストレスバーを追加した

--- Assets/Scripts/ColonistAI.cs
+++ Assets/Scripts/ColonistAI.cs
@@ -63,6 +63,21 @@ public class ColonistAI : MonoBehaviour
         get { return currentHealth; }
     }
 
+    public float GetHunger
+    {
+        get {
+            return hunger;
+        
+        }
+    }
+
+    public float GetStress
+    {
+        get { return stress;
+        }
+    }
+
+
     /// <summary>
     /// ”æ˜J‰ñ•œ‘¬“x
     /// </summary>
--- Assets/Scripts/ColonistHealthUI.cs
+++ Assets/Scripts/ColonistHealthUI.cs
@@ -11,11 +11,28 @@ public class ColonistHealthUI : MonoBehaviour
 
     public Image healthBar;
 
+    /// <summary>
+    /// ストレス値のバー
+    /// </summary>
+    public Image StressBar;
+
+    /// <summary>
+    /// 空腹値用のバー
+    /// </summary>
+    public Image HungerBar;
+
     // Update is called once per frame
     void Update()
     {
         //HealthBarに現在の体力/最大の体力で出る割合を表示
         healthBar.fillAmount = ColonistAI.GetCurrentHealth / ColonistAI.MaxHealth;
+
+        HungerBar.fillAmount 
+            =ColonistAI.GetHunger / 100; 
+
+
+        StressBar.fillAmount
+            = ColonistAI.GetStress / 100; 
 
         
     }
